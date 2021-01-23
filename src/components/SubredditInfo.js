d3ff430d826fb82e08b353e9676c3a3226801f79
sadikBr <sadbrah@gmail.com>
2021-01-24 00:21:35 +0100

    fixing styles

--- src/components/SubredditInfo.js
+++ src/components/SubredditInfo.js
@@ -3,6 +3,8 @@ const SubredditInfo = ({ subredditInfo }) => {
     const {
       banner_img,
       display_name,
+      banner_background_image,
+      community_icon,
       icon_img,
       public_description,
     } = subredditInfo;
@@ -10,13 +12,25 @@ const SubredditInfo = ({ subredditInfo }) => {
     return (
       <div className='container'>
         <div className='banner'>
-          {banner_img && <img src={banner_img} alt='banner' />}
+          {banner_img ? (
+            <img src={banner_img} alt='banner' />
+          ) : banner_background_image ? (
+            <img src={banner_background_image} alt='banner' />
+          ) : (
+            ''
+          )}
         </div>
         <div className='name'>
-          {icon_img && <img className='icon' src={icon_img} alt='icon' />}
+          {icon_img ? (
+            <img className='icon' src={icon_img} alt='icon' />
+          ) : community_icon ? (
+            <img className='icon' src={community_icon} alt='icon' />
+          ) : (
+            ''
+          )}
           <div className='sr-title'>
             <h2>{display_name}</h2>
-            <p>{public_description}</p>
+            {public_description && <p>{public_description}</p>}
           </div>
         </div>
       </div>
--- src/hooks/useAboutSubreddit.js
+++ src/hooks/useAboutSubreddit.js
@@ -16,19 +16,25 @@ const useAboutSubreddit = (subreddit) => {
       .then((response) => {
         const {
           banner_img,
+          banner_background_image,
+          community_icon,
           display_name,
           icon_img,
           public_description,
-          subscribers,
-          created_utc,
         } = response.data.data;
         setSubredditInfo({
           banner_img,
+          banner_background_image: banner_background_image.substr(
+            0,
+            banner_background_image.split('').indexOf('?')
+          ),
+          community_icon: community_icon.substr(
+            0,
+            community_icon.split('').indexOf('?')
+          ),
           display_name,
           icon_img,
           public_description,
-          subscribers,
-          created_utc,
         });
       })
       .catch(() => {
