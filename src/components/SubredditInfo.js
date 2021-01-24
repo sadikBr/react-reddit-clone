3500d7f1098f4e60851b57a6345fab537a56e48b
sadikBr <sadbrah@gmail.com>
2021-01-24 01:39:16 +0100

    fixing styles

--- src/components/SubredditInfo.js
+++ src/components/SubredditInfo.js
@@ -1,42 +1,39 @@
 const SubredditInfo = ({ subredditInfo }) => {
-  if (subredditInfo !== {}) {
-    const {
-      banner_img,
-      display_name,
-      banner_background_image,
-      community_icon,
-      icon_img,
-      public_description,
-    } = subredditInfo;
+  const {
+    banner_img,
+    display_name,
+    banner_background_image,
+    community_icon,
+    icon_img,
+    public_description,
+  } = subredditInfo;
 
-    return (
-      <div className='container'>
-        <div className='banner'>
-          {banner_img ? (
-            <img src={banner_img} alt='banner' />
-          ) : banner_background_image ? (
-            <img src={banner_background_image} alt='banner' />
-          ) : (
-            ''
-          )}
-        </div>
-        <div className='name'>
-          {icon_img ? (
-            <img className='icon' src={icon_img} alt='icon' />
-          ) : community_icon ? (
-            <img className='icon' src={community_icon} alt='icon' />
-          ) : (
-            ''
-          )}
-          <div className='sr-title'>
-            <h2>{display_name}</h2>
-            {public_description && <p>{public_description}</p>}
-          </div>
+  return subredditInfo.display_name ? (
+    <div className='container'>
+      <div className='banner'>
+        {banner_img ? (
+          <img src={banner_img} alt='banner' />
+        ) : banner_background_image ? (
+          <img src={banner_background_image} alt='banner' />
+        ) : (
+          ''
+        )}
+      </div>
+      <div className='name'>
+        {icon_img ? (
+          <img className='icon' src={icon_img} alt='icon' />
+        ) : community_icon ? (
+          <img className='icon' src={community_icon} alt='icon' />
+        ) : (
+          ''
+        )}
+        <div className='sr-title'>
+          <h2>{display_name}</h2>
+          {public_description && <p>{public_description}</p>}
         </div>
       </div>
-    );
-  }
-  return null;
+    </div>
+  ) : null;
 };
 
 export default SubredditInfo;
